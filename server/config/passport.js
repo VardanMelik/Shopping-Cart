23e2c04c61fbdf05232f5d1ad45a5dc0d05e8dc7
eyemotion <eyemotiongamestudio@gmail.com>
2019-06-11 14:38:25 +0400

    update

---
+++ server/config/passport.js
@@ -0,0 +1,32 @@
+const passport = require('passport')
+const User = require('../models/user')
+const LocalStrategy = require('passport-local').Strategy
+
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+})
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user)
+    })
+})
+
+passport.use('local.signup', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, (req, email, password, done) => {
+    User.findOne({ 'email': email },
+        (err, user) => {
+            if (err) {
+                return done(err)
+            }
+            if (user) {
+                return done(null, false, { message: 'Email is already in use.' })
+            }
+            let newUser = new User()
+            newUser.email = email
+            newUser.password = password
+        })
+}))
--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
 
 const userSchema = new Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true }
-})
+        email: { type: String, required: true },
+        password: { type: String, required: true }
+    })
+    //userSchema.methods
 
 module.exports = mongoose.model('User', userSchema)
